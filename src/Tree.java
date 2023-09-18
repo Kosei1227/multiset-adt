664f88364ca40151ed6b6597a00bf8df7ec63424
KoseiUemura <fruit.apple800@gmail.com>
2023-09-18 18:41:34 -0400

    Here is a constructor for Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,30 @@
-public class Tree {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Tree<T> {
+
+    // The item stored at this tree's root, or null if the tree is empty.
+    private T root;
+
+    // The list of all subtrees of this tree.
+    private List<Tree<T>> subtrees;
+
+    /**
+     * Initialize a new Tree with the given root value and subtrees.
+     * If root is null, the tree is empty.
+     * Precondition: if root is null, then subtrees is empty or null.
+     *
+     * @param root the root value
+     * @param subtrees the list of subtrees, can be null
+     */
+    public Tree(T root, List<Tree<T>> subtrees) {
+        this.root = root;
+        if (subtrees == null || root == null) {
+            this.subtrees = new ArrayList<>();
+        } else {
+            this.subtrees = new ArrayList<>(subtrees);
+        }
+    }
 }
+
